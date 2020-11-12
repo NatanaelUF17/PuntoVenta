7425aa2d18c81fec82228dd2c060fe362ed815f5
Natanael Ureña Fernandez <natanael_fernandez@ucne.edu.do>
2020-11-12 11:08:04 -0400

    Se creo el contexto de la base de datos y la base de datos

---
+++ PuntoVenta/AppDbContext/Context.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.EntityFrameworkCore;
+using PuntoVenta.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PuntoVenta.AppDbContext
+{
+    public class Context : DbContext
+    {
+        public Context(DbContextOptions<Context> options) : base (options) { }
+
+        public DbSet<Brand> Brands { get; set; }
+        public DbSet<Category> Categories { get; set; }
+        public DbSet<Client> Clients { get; set; }
+        public DbSet<Product> Products { get; set; }
+        public DbSet<Sale> Sales { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlite(@"Data Source=Data/punto_venta.db");
+        }
+    }
+}
