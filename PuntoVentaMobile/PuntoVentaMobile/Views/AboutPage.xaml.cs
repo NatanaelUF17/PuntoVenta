48039be1582ac9f32c0bd905c9187f2cd7ed67a6
Natanael Ureña Fernandez <natanael_fernandez@ucne.edu.do>
2020-11-18 15:20:09 -0400

    Se agregaron proyecto que se eliminaron posteriormente

---
+++ PuntoVentaMobile/PuntoVentaMobile/Views/AboutPage.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.ComponentModel;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace PuntoVentaMobile.Views
+{
+    public partial class AboutPage : ContentPage
+    {
+        public AboutPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ PuntoVentaMobile/PuntoVentaMobile/Views/NewItemPage.xaml.cs
@@ -0,0 +1,21 @@
+﻿using PuntoVentaMobile.Models;
+using PuntoVentaMobile.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace PuntoVentaMobile.Views
+{
+    public partial class NewItemPage : ContentPage
+    {
+        public Item Item { get; set; }
+
+        public NewItemPage()
+        {
+            InitializeComponent();
+            BindingContext = new NewItemViewModel();
+        }
+    }
+}
